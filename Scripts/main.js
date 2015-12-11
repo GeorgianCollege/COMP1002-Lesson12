2d278dcb1c3b072162516246d6e40b53ec0821d4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-12-11 09:46:27 -0500

    updated main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,11 +1,29 @@
-var button = document.getElementById("menu-button");
-var eventType = "click";
+// Variable Declarations
+var toggleButton = document.getElementById("menu-button");
+var mainHeading = document.getElementById("main-heading");
+var navBar = document.getElementById("nav-bar");
+var toggleButtonIsClicked = false;
 
+// add an event listener to the toggle button
+toggleButton.addEventListener("click", toggleButtonClicked);
+window.addEventListener("resize", resize);
 
-
-button.addEventListener(eventType, myFunction);
-
-function myFunction() {
-	console.log("my prettier function was clicked");
+// toggle button event handler
+function toggleButtonClicked() {
+	if (toggleButtonIsClicked) {
+		resize();	
+	} else {
+		mainHeading.innerHTML = "Toggle Button was clicked";
+		mainHeading.style.color = "red";
+		navBar.className = navBar.className + " responsive-nav";
+		toggleButtonIsClicked = true;
+	}
 }
+
+function resize() {
+		mainHeading.innerHTML = "Main Heading";
+		mainHeading.style.color = "black";
+		navBar.className = "nav-bar";
+		toggleButtonIsClicked = false;
+	}
     
