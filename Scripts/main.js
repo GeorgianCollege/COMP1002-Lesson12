3318bbcd8aea6dc3ded46abe3aab445685bf9242
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-12-11 08:47:35 -0500

    added javascript to main.js

---
+++ Scripts/main.js
@@ -0,0 +1,11 @@
+var button = document.getElementById("menu-button");
+var eventType = "click";
+
+
+
+button.addEventListener(eventType, myFunction);
+
+function myFunction() {
+	console.log("my prettier function was clicked");
+}
+    
